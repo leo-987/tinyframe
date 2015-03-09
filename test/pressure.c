28edaa10246273b5fb94175d529861a7274f8f75
Nestle <oyld0210@gmail.com>
2015-03-09 11:04:25 +0800

    Add pressure testing

--- test/pressure.c
+++ test/pressure.c
@@ -8,7 +8,10 @@ void request_handler(connection *conn)
 	static char buf[] = "HTTP/1.1 200 0K\r\nConnection:keep-alive\r\nContent-length:4\r\n\r\n"
 						"test";
 
+	/* 发送HTTP响应 */
 	connection_send(conn, buf, sizeof(buf) - 1);
+
+	/* 清空输入缓冲区 */
 	array_clear(conn->input_buffer);
 }
 
@@ -16,9 +19,13 @@ int main()
 {
 	server_manager *manager = server_manager_create();
 
+	/* 监听2016端口 */
 	inet_address addr = addr_create("any", 2016);
+
+	/* socket可读时调用request_handler函数 */
 	server *server = server_create(manager, addr, request_handler, NULL);
-	
+
+	/* 开始监听 */
 	server_manager_run(manager);
 
 	return 0;
